eded850275cfe7ab23655275e9e263bd98b83fba
chfour <chfourchfour@protonmail.com>
2024-03-31 21:41:53 +0200

    add k5prog to flake.nix

--- flake.nix
+++ flake.nix
@@ -9,10 +9,27 @@
         let
           pkgs = nixpkgs.legacyPackages.${system};
         in {
+          packages.k5prog = with pkgs; stdenv.mkDerivation {
+            pname = "k5prog";
+            version = "1.0.0";
+
+            src = fetchFromGitHub {
+              owner = "sq5bpf";
+              repo = "k5prog";
+              rev = "main";
+              hash = "sha256-uzhTYCpNjm/I+qlHUF6t9+hGjAvXFlzUM34SAxX5OjE=";
+            };
+
+            installPhase = ''
+              mkdir -p $out/bin/
+              cp k5prog $out/bin/k5prog
+            '';
+          };
           devShells.default = with pkgs; mkShell {
             nativeBuildInputs = [
               (python312.withPackages (p: with p; [ crcmod ]))
               gnumake gcc-arm-embedded-10
+              self.packages.${system}.k5prog
             ];
           };
         }
